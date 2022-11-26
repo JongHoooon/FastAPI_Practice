2657dcf84427a4fe8f2b4f29bc737c884e12a44e
seaLion3 <qnm541@gmail.com>
2022-11-26 11:07:03 +0900

    Response Model

--- blog/main.py
+++ blog/main.py
@@ -14,27 +14,13 @@ def get_db():
   finally:
     db.close
 
-@app.post('/blog', status_code = status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
-  new_blog = models.Blog(title = request.title, body = request.body)
-  db.add(new_blog)
-  db.commit()
-  db.refresh(new_blog)
-  return new_blog
-
-@app.delete('/blog/{id}', status_code = status.HTTP_204_NO_CONTENT)
-def destory(id, db: Session = Depends(get_db)):
-  blog = db.query(models.Blog).filter(models.Blog.id == 
-                                      id).delete(synchronize_session = False)
-  db.commit()
-  return 'done'
-
-@app.get('/blog')
+@app.get('/blog', response_model = list[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
   blogs = db.query(models.Blog).all()
   return blogs
 
-@app.get('/blog/{id}', status_code = 200)
+
+@app.get('/blog/{id}', status_code = 200, response_model = schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
   blog = db.query(models.Blog).filter(models.Blog.id == id).first()
   if not blog:
@@ -44,3 +30,34 @@ def show(id, response: Response, db: Session = Depends(get_db)):
       # response.status_code = status.HTTP_404_NOT_FOUND
       # return { 'detail': f'Blog with the id {id} is not available' }
   return blog
+
+
+@app.post('/blog', status_code = status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+  new_blog = models.Blog(title = request.title, body = request.body)
+  db.add(new_blog)
+  db.commit()
+  db.refresh(new_blog)
+  return new_blog
+
+
+@app.put('/blog/{id}', status_code = status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+  blog = db.query(models.Blog).filter(models.Blog.id == id)
+  if not blog.first():
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                        detail = f"Blog witdh id {id} not found")
+  blog.update(request.dict())
+  db.commit()
+  return 'updated'
+
+
+@app.delete('/blog/{id}', status_code = status.HTTP_204_NO_CONTENT)
+def destory(id, db: Session = Depends(get_db)):
+  blog = db.query(models.Blog).filter(models.Blog.id == id)
+  if not blog.first():
+    raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
+                        detail = f"Bolg witdh id {id} not found")
+  blog.delete(synchronize_session = False)                                    
+  db.commit()
+  return 'done'
