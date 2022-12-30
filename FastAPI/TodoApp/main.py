776f44a2844e1fe7b2b16ac571f83e4742aaf0b3
seaLion3 <qnm541@gmail.com>
2022-12-31 00:38:10 +0900

    API Request Methods

--- FastAPI/TodoApp/main.py
+++ FastAPI/TodoApp/main.py
@@ -51,15 +51,54 @@ async def create_todo(todo: Todo, db: Session = Depends(get_db)):
     db.add(todo_model)
     db.commit()
     
-    return {
-        "status": 201, 
-        "transaction": "Successful"
-    }
+    return successful_response(201)
     
     
 @app.put("/{todo_id}")
-async def up
+async def update_todo(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
+    todo_model: Todo = db.query(models.Todos)\
+        .filter(models.Todos.id == todo_id)\
+        .first()
+    
+    if todo_model is None: 
+        raise http_exception()
+    
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+    
+    db.add(todo_model)
+    db.commit()
+    
+    return successful_response(200)
+    
 
+@app.delete("/{todo_id}")
+async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos)\
+        .filter(models.Todos.id == todo_id)\
+        .first()
+        
+    if todo_model is None: 
+        raise http_exception()
+    
+    db.query(models.Todos)\
+        .filter(models.Todos.id == todo_id)\
+        .delete()
+        
+    db.commit()
+    
+    return successful_response(201)
+    
+
+def successful_response(status_code: int):
+    return {
+        "status": status_code, 
+        "transaction": "Successful"
+    }
+        
 
 def http_exception():
     return HTTPException(status_code=404, detail="Todo not found")
+
